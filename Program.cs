bd61148bfedc62074ce0840d8a0298609f34cacc
gundamss <chauyuhin1998@gmail.com>
2022-04-28 11:01:36 -0600

    commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace cse210_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomGenerator = new Random();
+            int rannum = randomGenerator.Next(1,1000);
+            int start_num = -1;
+            Console.WriteLine("Welcome to Tic Tac Toe");
+        }
+    }
+}
+    
